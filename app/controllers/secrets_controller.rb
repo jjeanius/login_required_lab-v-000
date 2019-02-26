61711ade169060bb1c913c654be11f32981dd7f1
Judy McGoldrick <jjeanius@verizon.net>
2019-02-26 02:28:07 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,14 @@
 class SecretsController < ApplicationController
+  before_action :require_login, only:[:new]
+
   def new
   end
+
+  def show
+    return head(:sessions) unless session.include? :name
+  #  @name = name.find_by(name: params[:name])
+
+  end
+
+
 end

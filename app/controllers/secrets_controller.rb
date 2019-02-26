4c2e39389cf3fe1d653253e17bfc4df426e3d317
Judy McGoldrick <jjeanius@verizon.net>
2019-02-26 04:49:47 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -13,7 +13,7 @@ class SecretsController < ApplicationController
   private
 
   def require_login
-    return head(3XX: redirect) unless session.include? :name
+    return head(:redirect) unless session.include? :name
   end
 
 end

f4f95763d1984d1417182c22a71d93c5cbd26901
Judy McGoldrick <jjeanius@verizon.net>
2019-02-26 02:36:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    session[:name] = params[:name]
+    params[:name] = session[:name]
       @name = session[:name]
   end
 
